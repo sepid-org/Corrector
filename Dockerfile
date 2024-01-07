267eb21c19057cb0273328fa9184c515b8080cf7
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2024-01-07 16:09:08 +0330

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# pull official base image
+FROM python:3.7
+
+# set work directory
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+# copy requirements
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+# copy entrypoint-prod.sh
+COPY ./entrypoint.prod.sh /app/entrypoint.prod.sh
+
+# copy project
+COPY . /usr/src/app/
+
+RUN adduser sepid
+
+RUN mkdir -p /app/logging && chown -R sepid /app/logging \
+	&& mkdir -p /app/media && chown -R sepid /app/media
+
+RUN chown -R sepid /usr/src/app/
+
+USER sepid
+
+EXPOSE 8000
+
+# run entrypoint.prod.sh
+ENTRYPOINT ["python", "mysite/manage.py"]
+CMD ["runserver", "0.0.0.0:8000"]
---
+++ corrector/settings/production.py
@@ -0,0 +1,81 @@
+from datetime import timedelta
+from corrector.settings.base import *
+
+DEBUG = get_environment_var('DEBUG', 'False') == 'True'
+
+SECRET_KEY = get_environment_var(
+    'SECRET_KEY', '*z!3aidedw32xh&1ew(^&5dgd17(ynnmk=s*mo=v2l_(4t_ff(')
+
+ALLOWED_HOSTS = get_environment_var('ALLOWED_HOSTS', '*').split(',')
+
+SERVICE_DOMAIN = 'https://corrector.sepid.org/'
+
+DB_NAME = get_environment_var('DB_NAME', 'workshop')
+DB_USER = get_environment_var('DB_USER', 'user')
+DB_PASS = get_environment_var('DB_PASS', 'p4s$pAsS')
+DB_HOST = get_environment_var('DB_HOST', 'localhost')
+DB_PORT = get_environment_var('DB_PORT', '5432')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, get_environment_var(
+    'STATIC_ROOT_FILE_NAME', 'staticfiles'))
+
+LOG_LEVEL = get_environment_var('LOG_LEVEL', 'INFO')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)-8s [%(module)s:%(funcName)s:%(lineno)d]: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'verbose',
+            'filename': os.path.join(BASE_DIR, 'logging/debug.log'),
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file', 'console'],
+            'level': LOG_LEVEL,
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['file', 'console'],
+            'level': LOG_LEVEL,
+            'propagate': True,
+        },
+        'corrector': {
+            'handlers': ['file', 'console'],
+            'level': LOG_LEVEL,
+            'propagate': True,
+        },
+    },
+}
+
+TESTING = False
+
+
+SWAGGER_URL = f'{SERVICE_DOMAIN}api/'
+
+CSRF_TRUSTED_ORIGINS = get_environment_var(
+    'CSRF_TRUSTED_ORIGINS', '*').split(',')
